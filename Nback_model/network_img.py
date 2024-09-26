1e0f46491793c6911be838795c1be189437f8c28
佐々木 光 <85730292+c0b2107561@users.noreply.github.com>
2024-09-26 12:23:44 +0900

    Update network_img.py

--- Nback_model/network_img.py
+++ Nback_model/network_img.py
@@ -1,25 +1,28 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def Watts_Strogats_small_world_graph():
-    n_nodes = 100 # ノード数
-    k_neighbors = 6 # 隣接ノード数
-    p_rewire = 1 # 再配線確率
-
-    # n_nodes = 20 # ノード数Watts
-    # k_neighbors = 4 # 隣接ノード数
-    # p_rewire = 1 # 再配線確率
-
+def Watts_Strogats_small_world_graph(n_nodes, k_neighbors, p_rewire):
 
     ws_graph = nx.watts_strogatz_graph(n_nodes, k_neighbors, p_rewire)
     return ws_graph
 
 def graph_plot(graph):
     pos = nx.circular_layout(graph)
-    nx.draw(graph, pos, with_labels=True, font_weight='bold')
+    # nx.draw(graph, pos, with_labels=True, font_weight='bold')
+    # plt.show()
+
+    nx.draw(graph, with_labels=True, font_weight='bold')
     plt.show()
 
 if __name__ == "__main__":
-    G = Watts_Strogats_small_world_graph()
+    n_nodes = 100 # ノード数
+    k_neighbors = 20 # 隣接ノード数
+    p_rewire = 1 # 再配線確率
+
+    # n_nodes = 20 # ノード数Watts
+    # k_neighbors = 4 # 隣接ノード数(最初の接続数,rewireによって接続数は変化)
+    # p_rewire = 0.3 # 再配線確率
+
+    G = Watts_Strogats_small_world_graph(n_nodes, k_neighbors, p_rewire)
     graph_plot(G)
 
